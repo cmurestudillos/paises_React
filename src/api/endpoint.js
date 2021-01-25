0149dadbd374a27953eab43476df7ab0b1bcf1fa
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-01-25 08:08:57 +0100

    SNV del proyecto

---
+++ src/api/endpoint.js
@@ -0,0 +1,8 @@
+//----------------------------------------------------------------------//
+// Configuracion global de nuestra aplicacion                           //
+//----------------------------------------------------------------------//
+var url = { 
+    endpoint: "https://restcountries.eu/rest/v2/lang/es"
+}
+
+export default url;
